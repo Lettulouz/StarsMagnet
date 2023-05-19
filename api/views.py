10984d6b4fa28bd93fd4ec205035bfcaa7454c31
tentegess <tenagent2222@gmail.com>
2023-05-19 18:03:18 +0200

    Registration alpha

--- api/views.py
+++ api/views.py
@@ -3,7 +3,23 @@ import json
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
+from .serializers.RegisterSerializer import RegisterSerializer
+
 # Create your views here.
 @api_view(["GET","POST"])
 def test(request, *args, **kwargs):
     return Response({'hehe':'papież tańczy'})
+
+@api_view(['POST'])
+def register(request, *args, **kwargs):
+    serializer = RegisterSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        account = serializer.save()
+        data['response']="successfully registered a new user"
+    else:
+        data = serializer.errors
+    return Response(data)
+
+
