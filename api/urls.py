d64626bb73f1da80215ea489e94f9bd18e1c38c2
tentegess <tenagent2222@gmail.com>
2023-05-20 02:58:06 +0200

    token auth alpha

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
     path('', views.test),
-    path('register', views.register)
+    path('register', views.register),
+    path('login', obtain_auth_token)
 ]
