df8508af261061719a3892a85555ade3db003b2b
kBaginskik <korniszonba@gmail.com>
2023-06-04 22:28:21 +0200

    company list added categories and pagination to opinions

--- api/models/categories.py
+++ api/models/categories.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     icon = models.CharField(max_length=55)
 
     class Meta:
--- api/urls.py
+++ api/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('opinion', views.opinion),
     path('category', views.categories),
     path('category/<int:pk>', views.categories),
-    path('category/pageable', views.category_pagable),
-    path('category/pageable/<int:amount>', views.category_pagable),
+    path('category/pageable', views.category_pageable),
+    path('category/pageable/<int:amount>', views.category_pageable),
 
     path('login', views.login),
     path('token/refresh', views.refresh_token),
