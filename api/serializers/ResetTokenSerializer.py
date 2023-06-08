341b753b6967ec45b26281e5165c70e108d3a1f2
milosz08 <gilgamilosz451@gmail.com>
2023-06-08 02:51:41 +0200

    remove dots and add response field

--- api/serializers/ResetTokenSerializer.py
+++ api/serializers/ResetTokenSerializer.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse
 from rest_framework import serializers
 from api.models import Safety_Words
-from api.models import Companies
 from django.db.models import Q
-import json
 from ..utils.generate_token import generate_token
 from ..utils.generate_safe_words import generate_safe_words
 from ..utils.generate_safe_words import make_dictio
@@ -28,10 +25,10 @@ class ResetTokenSerializer(serializers.Serializer):
 
         matched_object = Safety_Words.objects.filter(queries).first()
         if not matched_object:
-            raise serializers.ValidationError({"data": "Invalid data. Error code A01."})
+            raise serializers.ValidationError({"data": "Invalid data. Error code A01"})
 
         if matched_object.user.email != user and matched_object.user.username != user:
-            raise serializers.ValidationError({"data": "Invalid data. Error code A02."})
+            raise serializers.ValidationError({"data": "Invalid data. Error code A02"})
 
         token = generate_token()
 
@@ -47,9 +44,10 @@ class ResetTokenSerializer(serializers.Serializer):
             raise serializers.ValidationError({"data": "An error has occurred, no data has been changed"})
 
         if matched_object:
-            return {'words': json_response, 'token': token}
+            return {'responseWords': json_response, 'token': token,
+                    'response': "Your token and secure words was successfully changed" }
 
-        raise serializers.ValidationError({"data": "Invalid data. Error code A03."})
+        raise serializers.ValidationError({"data": "Invalid data. Error code A03"})
 
     def create(self, validated_data):
         pass
