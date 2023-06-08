290ab056b216ed05bd975979034eefeed20c6f09
milosz08 <gilgamilosz451@gmail.com>
2023-06-08 02:44:07 +0200

    reorganized and add comments

--- api/urls.py
+++ api/urls.py
@@ -5,24 +5,40 @@ from . import views
 
 urlpatterns = [
     path('', views.test),
-    path('register', views.register),
-    path('company', views.company),
-    path('company/<int:pk>', views.company),
-    path('opinion', views.opinion),
-    path('companyOpinion', views.company_opinion),
-    path('category', views.categories),
-    path('category/', views.companies_of_category, name="category"),
-    path('category/pageable', views.category_pageable),
-    path('company/pageable', views.company_pageable, name="query"),
-    path('category/pageable/<int:amount>', views.category_pageable),
-    path('category/company/pageable', views.company_category_pageable, name="category"),
-    path('search/', views.search_companies, name="query"),
-    path('opinionList/company/<int:company_id>', views.list_company_opinions),
-    path('opinionList/company/<int:company_id>/pageable/<int:amount>', views.company_opinions_pageable),
-    path('reset/token', views.reset_token),
 
+    # auth
     path('login', views.login),
-    path('login/auto', views.auto_login),
+    path('register', views.register),
     path('company/login', views.login_company),
+    path('token/reset', views.reset_token),
     path('token/refresh', views.refresh_token),
+    path('login/auto', views.auto_login),
+
+    # add company
+    path('company', views.company),
+
+    # get company by id
+    path('company/<int:pk>', views.company),
+
+    # add opinion by user
+    path('opinion', views.opinion),
+
+    # add response by company
+    path('opinion/company', views.company_opinion),
+
+    # get all opinions with pageable
+    path('opinion-list/company/<int:company_id>', views.list_company_opinions),
+    path('opinion-list/company/<int:company_id>/pageable/<int:amount>', views.company_opinions_pageable),
+
+    # get all categories
+    path('category/pageable/<int:amount>', views.category_pageable),
+    path('category', views.categories),
+
+    # get all company's by selected category
+    path('category/company', views.companies_of_category, name="category"),
+    path('category/company/pageable', views.company_category_pageable, name="category"),
+
+    # get all company's by query
+    path('search', views.search_companies, name="query"),
+    path('company/pageable', views.company_pageable, name="query"),
 ]
