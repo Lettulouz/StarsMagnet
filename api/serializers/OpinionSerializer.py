d48b3db48b642ccc78837e0f04475904cb1fd5d4
milosz08 <gilgamilosz451@gmail.com>
2023-06-10 08:16:55 +0200

    add fullname and username fields

--- api/serializers/OpinionSerializer.py
+++ api/serializers/OpinionSerializer.py
@@ -1,24 +1,37 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from api.models import Opinions
-
+User = get_user_model()
 
 class OpinionSerializer(serializers.ModelSerializer):
     ratingDate = serializers.DateTimeField(source='rating_date')
     commentDate = serializers.DateTimeField(source='comment_date')
     responseDate = serializers.DateTimeField(source='response_date')
     companyResponse = serializers.CharField(source='company_response')
-    userId = serializers.CharField(source='user_id')
-    companyId = serializers.CharField(source='company_id')
+    userId = serializers.IntegerField(source='user_id')
+    username = serializers.SerializerMethodField()
+    fullname = serializers.SerializerMethodField()
 
     class Meta:
         model = Opinions
-        fields = ("userId",
-                  'companyId',
+        fields = ["userId",
+                  "username",
+                  "fullname",
                   'rating',
                   "ratingDate",
                   'comment',
                   'commentDate',
                   'companyResponse',
-                  "responseDate")
+                  "responseDate"]
 
+    def get_fullname(self, obj):
+        user = User._default_manager.filter(id=obj.user_id).first()
+        if user:
+            return f"{user.first_name} {user.last_name}"
+        return ""
 
+    def get_username(self, obj):
+        user = User._default_manager.filter(id=obj.user_id).first()
+        if user:
+            return user.username
+        return ""
